9f5a5b587d8edc773cfa5c95259e2eba4b4161d6
Mike Burke <mikeburke.214@gmail.com>
2019-06-02 13:13:29 +0100

    Added Arrays::getByKeyPath()

--- src/Arrays.php
+++ src/Arrays.php
@@ -16,6 +16,53 @@ class Arrays
   }
 
   /**
+   * @param string $keyPath
+   * @param array $array
+   * @param string $keyPathDelimiter
+   * @param mixed|null $default
+   * @return mixed|null
+   */
+  public static function getByKeyPath(
+    string $keyPath,
+    array $array,
+    string $keyPathDelimiter = ':',
+    $default = null
+  )
+  {
+    $keyPathArray = explode($keyPathDelimiter, $keyPath);
+
+    foreach ($keyPathArray as $key)
+    {
+      array_shift($keyPathArray);
+
+      if (isset($array[$key]))
+      {
+        // Search finished
+        if (empty($keyPathArray))
+        {
+          // Search finished
+          if (!isset($array[$key]))
+          {
+            return $default;
+          }
+
+          return $array[$key];
+        }
+        else
+        {
+          return self::getByKeyPath(implode($keyPathDelimiter, $keyPathArray), $array[$key]);
+        }
+      }
+      else
+      {
+        return $default;
+      }
+    }
+
+    return $default;
+  }
+
+  /**
    * @param array $array
    * @return array
    */
