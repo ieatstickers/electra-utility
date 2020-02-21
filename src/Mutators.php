1186f7d9607a6985088bfdc62d4989260a81d2d5
Mike Burke <mikeburke.214@gmail.com>
2020-02-21 20:52:30 +0000

    Added mutators

--- src/Mutators.php
+++ src/Mutators.php
@@ -7,33 +7,50 @@ use Carbon\Carbon;
 class Mutators
 {
   /** @return callable */
-  public static function dateToCarbon(): callable
+  public static function carbonToDateString(): callable
   {
-    return function($date)
-    {
-      if ($date instanceof Carbon || is_null($date)) return $date;
-      return new Carbon($date);
+    return function($carbon) {
+      if ($carbon instanceof Carbon)
+      {
+        return $carbon->toDateString();
+      }
+
+      return $carbon;
     };
   }
 
   /** @return callable */
-  public static function dateTimeToCarbon(): callable
+  public static function dateStringToCarbon(): callable
+  {
+    return function($dateString)
+    {
+      if ($dateString instanceof Carbon || is_null($dateString)) return $dateString;
+      return new Carbon($dateString);
+    };
+  }
+
+  /** @return callable */
+  public static function carbonToDateTimeString(): callable
+  {
+    return function($carbon)
+    {
+      if ($carbon instanceof Carbon)
+      {
+        return $carbon->toDateTimeString();
+      }
+
+      return $carbon;
+    };
+  }
+
+  /** @return callable */
+  public static function dateTimeStringToCarbon(): callable
   {
     return function($dateTime)
     {
       if ($dateTime instanceof Carbon || is_null($dateTime)) return $dateTime;
       return new Carbon($dateTime);
     };
-  }
-
-  /** @return callable */
-  public static function carbonToDateTime(): callable
-  {
-    return function($carbon) {
-      if ($carbon instanceof Carbon || is_null($carbon)) return $carbon;
-      return new Carbon($carbon);
-    };
-
   }
 
 }
