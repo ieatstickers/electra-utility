a4253f92b3c3f63758fb48bdbceb00854b32237a
mikeburke <mikeburke.214@gmail.com>
2023-02-19 19:00:27 +0000

    Bug fix for Money.getFormattedPrice

--- src/Money.php
+++ src/Money.php
@@ -10,6 +10,12 @@ class Money
    */
   public static function getFormattedPrice(float $value): string
   {
+    if ($value < 0)
+    {
+      $formattedValue = number_format($value * -1, 2);
+      return "-£$formattedValue";
+    }
+
     $formattedValue = number_format($value, 2);
     return "£$formattedValue";
   }
